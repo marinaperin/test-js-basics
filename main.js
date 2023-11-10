a79677617246b1f7aec63e58488b69311eb65db0
Marina Perin <maryfrancisco@live.com>
2023-11-10 10:54:00 +0100

    feat | exercise 2

--- main.js
+++ main.js
@@ -17,4 +17,31 @@ const analyzeText = (string) => {
   });
 };
 
+// Exercise 2
 
+const fillArray = (numCount, min, max) => {
+    let num = [];
+    for (let i = 0; i < numCount; i++) {
+      const n = Math.floor(Math.random() * (max - min + 1) + min);
+      num.push(n);
+    }
+    return num;
+  };
+  
+  const countOddOrEven = (array) => {
+    let oddOrEven = {};
+    let oddTotal = 0;
+    let evenTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+      array[i] % 2 === 0 ? evenTotal++ : oddTotal++;
+    }
+    return (oddOrEven = {
+      odd: oddTotal,
+      even: evenTotal,
+    });
+  };
+
+
+  // Exercise 3
+
+  
